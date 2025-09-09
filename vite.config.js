f0c7768621d185af7466496526c99cf49b0e8153
Alexandr <104072970+Dennchik@users.noreply.github.com>
2025-09-09 21:00:38 +0300

    09.09.2025.v21-00

--- vite.config.js
+++ vite.config.js
@@ -49,8 +49,8 @@ export default defineConfig(({ command }) => {
 
         locals: {
           productsMap,
-          // webRoot: isProd ? '../' : './',
-          webRoot: '../',
+          webRoot: isProd ? './' : '/',
+          // webRoot: '../',
           news,
           about,
           partners,
